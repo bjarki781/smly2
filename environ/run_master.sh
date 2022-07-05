d8eddad51ea41b7b55d18adf01ab776baad500e5
Bjarki Gunnarsson <bjg60@hi.is>
2022-07-05 10:31:30 +0000

    add some more files

---
+++ environ/run_master.sh
@@ -0,0 +1,1 @@
+./target/release/node-template --base-path /home/bjarki/blockchain --chain ./customSpecRaw.json --port 30333 --ws-port 9945 --rpc-port 9934 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name NUC --password mamma --rpc-methods=unsafe

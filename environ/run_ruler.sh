a550650643596df3939643766dea9c18fc5f0cb1
Bjarki Gunnarsson <bjg60@hi.is>
2022-07-06 11:59:34 +0000

    fix ruler script

--- environ/run_ruler.sh
+++ environ/run_ruler.sh
@@ -1,1 +1,4 @@
-./target/release/node-template --base-path /home/bjarki/blockchain --chain ./customSpecRaw.json --port 30333 --ws-port 9945 --rpc-port 9934 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name NUC --password mamma --rpc-methods=unsafe
+#!/bin/bash
+
+target/release/node-template --base-path ~/blockchain --chain environ/customSpecRaw.json --port 30333 --ws-port 9945 --rpc-port 9934 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name NUC --password mamma --rpc-methods=unsafe
+

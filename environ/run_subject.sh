eda79d140a6d3843fa54e16f328d2787198f5dbc
bjg60@hi.is <bjg60@hi.is>
2022-07-06 12:00:17 +0000

    fix subject script

--- environ/run_subject.sh
+++ environ/run_subject.sh
@@ -1,6 +1,6 @@
-substrate-node-template/target/release/node-template --base-path ./blockchain \
---chain ./customSpecRaw.json --port 30334 --ws-port 9946 --rpc-port 9934 --telemetry-url \
-"wss://telemetry.polkadot.io/submit/ 0" --validator --name BJARKI --password mamma \
+target/release/node-template --base-path ~/blockchain \
+--chain environ/customSpecRaw.json --port 30334 --ws-port 9946 --rpc-port 9934 --telemetry-url \
+"wss://telemetry.polkadot.io/submit/ 0" --validator --name "`hostname`" --password mamma \
 --rpc-cors=all --unsafe-ws-external --unsafe-rpc-external \
 --bootnodes=/ip4/130.208.71.25/tcp/30333/p2p/12D3KooWLWAtBCH1ofFDqf8ACNzoNkYkWcrZ5rpAmtG2j5kAWrNh 
 

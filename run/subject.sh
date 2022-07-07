6643e5f2a04ac9e2f0e051a52ab4837f2dd81b2b
Bjarki Gunnarsson <bjg60@hi.is>
2022-07-07 14:47:15 +0000

    rename files and reorganize

--- run/subject.sh
+++ run/subject.sh
@@ -1,5 +1,5 @@
 target/release/smly2 --base-path ~/blockchain \
---chain environ/customSpecRaw.json --port 30334 --ws-port 9946 --rpc-port 9934 --telemetry-url \
+--chain run/smly2SpecRaw.json --port 30334 --ws-port 9946 --rpc-port 9934 --telemetry-url \
 "wss://telemetry.polkadot.io/submit/ 0" --validator --name "`hostname`" --password mamma \
 --rpc-cors=all --unsafe-ws-external --unsafe-rpc-external \
 --bootnodes=/ip4/130.208.71.25/tcp/30333/p2p/12D3KooWLWAtBCH1ofFDqf8ACNzoNkYkWcrZ5rpAmtG2j5kAWrNh 

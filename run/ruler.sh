78bb61db79ff27e5b20ec1ff49ddca856c37a270
bjg60@hi.is <bjg60@hi.is>
2022-07-18 10:11:49 +0000

    remove unsafeness from node run files

--- run/ruler.sh
+++ run/ruler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-target/release/smly2 --base-path ~/blockchain --chain run/smly2SpecRaw.json --port 30333 --ws-port 9945 --rpc-port 9934 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name NUC --password mamma --rpc-methods=unsafe
+target/release/smly2 --base-path ~/blockchain --chain run/smly2SpecRaw.json --port 30333 --ws-port 9945 --rpc-port 9934 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name "`hostname`" --password mamma
 

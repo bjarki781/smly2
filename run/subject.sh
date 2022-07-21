534754aceb4655931ab0f946d7f0baa1163be617
bjg60@hi.is <bjg60@hi.is>
2022-07-21 16:32:35 +0000

    some fixes for the run files

--- run/subject.sh
+++ run/subject.sh
@@ -1,5 +1,4 @@
-target/release/smly2 --base-path ~/blockchain \
---chain run/smly2SpecRaw.json --port 30335 --ws-port 9946 --rpc-port 9936 --telemetry-url \
-"wss://telemetry.polkadot.io/submit/ 0" --validator --name "`hostname`" --password mamma \
---bootnodes=/ip4/130.208.71.25/tcp/30333/p2p/12D3KooWNL9pe2f6i4mKQjwYGNUnDpTPK8dQiyv88EGhVwzFyfrV 
+#!/bin/bash
+
+target/release/smly2 --base-path ~/blockchain --chain run/smly2SpecRaw.json --port 30335 --ws-port 9946 --rpc-port 9936 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name "`hostname`" --password mamma --bootnodes=/ip4/130.208.71.25/tcp/30334/p2p/12D3KooWLoNXETTfLuDL7yQFp2wKiCLZrXsXx9FJUrcQXz1hAVBy
 

d51cee8ff075cae5078262b633c88374df4db052
Bjarki Gunnarsson <bjg60@hi.is>
2022-07-21 15:43:49 +0000

    add rpc-cors flag to run/access

--- run/access.sh
+++ run/access.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-target/release/smly2 --base-path ~/blockchain-access --chain run/smly2SpecRaw.json --port 30333 --ws-port 9944 --rpc-port 9934 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --name "`hostname`-access" --password mamma --bootnodes=/ip4/130.208.71.25/tcp/30333/p2p/12D3KooWNL9pe2f6i4mKQjwYGNUnDpTPK8dQiyv88EGhVwzFyfrV
+target/release/smly2 --base-path ~/blockchain-access --chain run/smly2SpecRaw.json --port 30333 --ws-port 9944 --rpc-port 9934 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --rpc-cors=all --name "`hostname`-access" --password mamma --bootnodes=/ip4/130.208.71.25/tcp/30334/p2p/12D3KooWNL9pe2f6i4mKQjwYGNUnDpTPK8dQiyv88EGhVwzFyfrV
 
